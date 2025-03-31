9cc33364a6a337750eecaf587fa553cd41e16e6b
Gohlub <62673775+Gohlub@users.noreply.github.com>
2025-03-31 10:58:41 -0400

    removed FE boilerplate

--- src/new/templates/ui/llm-template/ui/src/App.tsx
+++ src/new/templates/ui/llm-template/ui/src/App.tsx
@@ -356,17 +356,6 @@ function App() {
           </div>
         </div>
       </div>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   );
 }
