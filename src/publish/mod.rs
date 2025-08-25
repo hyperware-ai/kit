93730bf23276c185c8b14dc85ac6868775127f4f
hosted-fornet <hosted-fornet@protonmail.com>
2025-08-25 16:52:47 -0700

    publish: print metadata hash for debugging purposes

--- src/publish/mod.rs
+++ src/publish/mod.rs
@@ -421,6 +421,7 @@ pub async fn execute(
     }
 
     let metadata_hash = check_remote_metadata(&metadata, metadata_uri, package_dir).await?;
+    info!("metadata_hash: {metadata_hash}");
     if !unpublish {
         check_pkg_hash(&metadata, package_dir, metadata_uri)?;
     }
